af00de181787e050fccfcb727a12cb72798e28fe
gmun <garrettohrt@gmail.com>
2022-11-06 21:20:30 -0600

    add getName test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,18 @@
 const Employee = require("../lib/Employee");
+const employee = new Employee("garrett", "234", "go@go.com");
 
 describe("Employee", () => {
     it("should get the constructor values for the employee object", () => {
-        const employee = new Employee("garrett", "234", "go@go.com");
+       
 
         expect(employee.name).toBe("garrett");
         expect(employee.id).toBe("234");
         expect(employee.email).toBe("go@go.com");
+    })
+});
+
+describe("getName", () => {
+    it("it should return the name from the employee object", () => {
+        expect(employee.getName()).toBe("garrett");
     })
 });
