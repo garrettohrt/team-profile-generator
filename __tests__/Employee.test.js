f670f966410c8b31736308281103addc54937001
gmun <garrettohrt@gmail.com>
2022-11-06 21:26:32 -0600

    finish Employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -12,7 +12,31 @@ describe("Employee", () => {
 });
 
 describe("getName", () => {
-    it("it should return the name from the employee object", () => {
+    it("should return the name from the employee object", () => {
+
         expect(employee.getName()).toBe("garrett");
     })
 });
+
+describe("getId", () => {
+    it("should return the id from the employee object", () => {
+
+        expect(employee.getId()).toBe("234");
+    })
+});
+
+describe("getEmail", () => {
+    it("should return the email from the employee object", () => {
+
+        expect(employee.getEmail()).toBe("go@go.com");
+    })
+});
+
+describe("getRole", () => {
+    it("should return the string Employee", () => {
+
+        expect(employee.getRole()).toBe("Employee");
+    })
+})
+
+
