cd73019439812121a43ed181ac8faa8a4e5c442e
gmun <garrettohrt@gmail.com>
2022-11-08 09:52:18 -0600

    finish tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,41 +1,40 @@
 const Employee = require("../lib/Employee");
 const employee = new Employee("garrett", "234", "go@go.com");
 
-describe("Employee", () => {
+describe("Employee test", () => {
     it("should get the constructor values for the employee object", () => {
        
-
-        expect(employee.name).toBe("garrett");
-        expect(employee.id).toBe("234");
-        expect(employee.email).toBe("go@go.com");
+        expect(employee.name).toEqual("garrett");
+        expect(employee.id).toEqual("234");
+        expect(employee.email).toEqual("go@go.com");
     })
 });
 
 describe("getName", () => {
     it("should return the name from the employee object", () => {
 
-        expect(employee.getName()).toBe("garrett");
+        expect(employee.getName()).toEqual("garrett");
     })
 });
 
 describe("getId", () => {
     it("should return the id from the employee object", () => {
 
-        expect(employee.getId()).toBe("234");
+        expect(employee.getId()).toEqual("234");
     })
 });
 
 describe("getEmail", () => {
     it("should return the email from the employee object", () => {
 
-        expect(employee.getEmail()).toBe("go@go.com");
+        expect(employee.getEmail()).toEqual("go@go.com");
     })
 });
 
 describe("getRole", () => {
     it("should return the string Employee", () => {
 
-        expect(employee.getRole()).toBe("Employee");
+        expect(employee.getRole()).toEqual("Employee");
     })
 })
 
---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,42 @@
+const Engineer = require("../lib/Engineer");
+const engineer = new Engineer("garrett", "234", "go@go.com", "garrettohrt");
+
+describe("Engineer test", () => {
+    it("should get the constructor values for the engineer object", () => {
+       
+        expect(engineer.name).toEqual("garrett");
+        expect(engineer.id).toEqual("234");
+        expect(engineer.email).toEqual("go@go.com");
+        expect(engineer.github).toEqual("garrettohrt")
+    })
+
+    
+        it("should return the name from the engineer object", () => {
+    
+            expect(engineer.getName()).toEqual("garrett");
+        })
+   
+    
+    
+        it("should return the id from the engineer object", () => {
+    
+            expect(engineer.getId()).toEqual("234");
+        })
+    
+    
+    
+        it("should return the email from the engineer object", () => {
+    
+            expect(engineer.getEmail()).toEqual("go@go.com");
+        })
+    
+    
+  
+        it("should return the string Engineer", () => {
+    
+            expect(engineer.getRole()).toEqual("Engineer");
+        })
+   
+    
+});
+
